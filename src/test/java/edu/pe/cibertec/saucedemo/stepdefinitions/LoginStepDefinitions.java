360c0663d9f3cc047a23e0d749c581b70083e117
Banner Alfredo Cosanotan Rodriguez <doradooficial177@gmail.com>
2026-04-15 23:24:24 -0500

    Pregunta 4

--- src/test/java/edu/pe/cibertec/saucedemo/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/edu/pe/cibertec/saucedemo/stepdefinitions/LoginStepDefinitions.java
@@ -7,14 +7,22 @@ import edu.pe.cibertec.saucedemo.tasks.OpenTheLoginPage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.en.And;
+
+import com.microsoft.playwright.Page;
+
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.playwright.abilities.BrowseTheWebWithPlaywright;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.hamcrest.Matchers.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class LoginStepDefinitions {
+
+    private long startTime;
+    private long endTime;
 
     @Given("{word} is on the SauceDemo login page")
     public void openLoginPage(String actorName) {
@@ -53,7 +61,59 @@ public class LoginStepDefinitions {
 
     @Then("she should remain on the login page")
     public void shouldRemainOnTheLoginPage() {
-
+        OnStage.theActorInTheSpotlight().should(
+                seeThat(ThePageTitle.displayed(), not(equalTo("Products")))
+        );
     }
 
+    @When("she logs in (performance) with username {string} and password {string}")
+    public void loginPerformance(String username, String password) {
+
+        startTime = System.currentTimeMillis();
+
+        OnStage.theActorInTheSpotlight().attemptsTo(
+                LoginAs.user(username).withPassword(password)
+        );
+
+        endTime = System.currentTimeMillis();
+    }
+
+    @And("the page load time should be greater than {int} milliseconds")
+    public void validarTiempo(int tiempo) {
+        long total = endTime - startTime;
+
+        System.out.println("Tiempo de carga: " + total);
+
+        if (total <= tiempo) {
+            throw new AssertionError("El tiempo fue menor al esperado");
+        }
+    }
+
+    @And("she navigates to the cart page")
+    public void irCarrito() {
+        Page page = OnStage.theActorInTheSpotlight()
+                .abilityTo(BrowseTheWebWithPlaywright.class)
+                .getCurrentPage();
+
+        page.locator(".shopping_cart_link").click();
+    }
+
+    @And("she navigates back to the inventory page")
+    public void volverInventario() {
+        Page page = OnStage.theActorInTheSpotlight()
+                .abilityTo(BrowseTheWebWithPlaywright.class)
+                .getCurrentPage();
+
+        page.goBack();
+    }
+
+    @Then("she should still be logged in")
+    public void validarSesion() {
+        Page page = OnStage.theActorInTheSpotlight()
+                .abilityTo(BrowseTheWebWithPlaywright.class)
+                .getCurrentPage();
+
+        assertThat(page.locator(".title")).isVisible();
+        assertThat(page.locator(".title")).containsText("Products");
+    }
 }
